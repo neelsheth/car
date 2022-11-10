8a8021763089ac7f03d0e67a0450b9606a1dd194
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2022-11-10 22:28:58 +0530

    Add files via upload

---
+++ src/components/LaptopBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from './docs/logo.svg'
+
+
+///displaying Each Index data as individual component
+export default function LaptopBox(props) {
+  const data = props.data
+
+  return (
+    <div className='box' style={{marginRight: '45px'}} >
+      <div className='capital text'>{data.bodyType}</div>
+      <div className='text'><span style={{marginRight:'10px' }}>{data.modelName}</span>  {data.modelType}</div>
+      <img className='mainImage' src={data.imageUrl} height="250px"></img>
+      <div className='links'>
+      <button className='btn'>LEARN
+          <img className='logo' src={logo} height="20px"></img>
+          </button>
+          <button className='btn'>SHOP
+          <img className='logo' src={logo} height="20px"></img>
+          </button>
+      </div>
+    </div>
+  )
+}
