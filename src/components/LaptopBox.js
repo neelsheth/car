31b69ebea7ee2aa2542b66da709b2f81ddde5c15
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2022-11-11 12:01:11 +0530

    Update LaptopBox.js

--- src/components/LaptopBox.js
+++ src/components/LaptopBox.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import logo from './docs/logo.svg'
 
 
 ///displaying Each Index data as individual component
 export default function LaptopBox(props) {
   const data = props.data
+  const [popUp, setPopUp] = useState(false);
+  const popUpClose = ()=>{
+    setPopUp(false)
+  }
+
 
   return (
     <div className='box' style={{marginRight: '55px'}} >
@@ -12,13 +17,14 @@ export default function LaptopBox(props) {
       <div className='text'><span style={{marginRight:'10px' }}>{data.modelName}</span>  {data.modelType}</div>
       <img className='mainImage' src={data.imageUrl} height="250px"></img>
       <div className='links'>
-      <button className='btn'>LEARN
+      <button className='btn' onClick={()=>setPopUp(true)}>LEARN
           <img className='logo' src={logo} height="20px"></img>
           </button>
           <button className='btn'>SHOP
           <img className='logo' src={logo} height="20px"></img>
           </button>
       </div>
+      {popUp && <PopUp data={data} popUpClose={popUpClose}></PopUp>}
     </div>
   )
 }
